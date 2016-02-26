9eff3e98af6ded3a2cb183b784e3b4242718c1b2
Yesenia Bastian <yeseniabastian223@gmail.com>
2016-02-26 11:48:11 -0800

    first upload

---
+++ app/build/generated/source/buildConfig/debug/com/yesenia/android/stopwatch/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.yesenia.android.stopwatch;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.yesenia.android.stopwatch";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
